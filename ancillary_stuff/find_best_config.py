17d92f13d2891a4d68deaef1ac2975983fcadd64
Arya Vishe (Ajit Rajwade Server 1) <aryavishe@iitb.ac.in>
2025-03-24 21:51:25 +0530

    Added the ability to quickly find best cases

--- ancillary_stuff/find_best_config.py
+++ ancillary_stuff/find_best_config.py
@@ -14,6 +14,8 @@ def main():
     for file in results_files:
         results_df = pd.read_csv(os.path.join(search_dir, file))
         local_max_precision = results_df['precision'].max()
+        naive_time = results_df['naive_running_time'][3]
+        flinng_time = results_df['flinng_running_time'][3]
         # it is implied in this case that precision = recall
 
         if local_max_precision > max_precision:
@@ -21,8 +23,11 @@ def main():
             max_precision_file = file
             max_precision_r = file.split('_')[0]
             max_precision_w = file.split('_')[1]
+            relevant_naive_time = naive_time
+            relevant_flinng_time = flinng_time
 
-    print(f"The best configuration is {max_precision_r} and {max_precision_w} with a precision of {max_precision}")
+    print(f"The best configuration is r = {max_precision_r} and w = {max_precision_w} with a precision of {max_precision}")
+    print(f"For that case, exhaustive search took {relevant_naive_time} ms and FLINNG took {relevant_flinng_time} ms")
 
 if __name__=="__main__":
     main()
